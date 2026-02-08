cd2478bfc164a978baded33d693fb3c5c0b9b489
Oluwafunke Adeyemo <adey0038@algonquinlive.com>
2026-02-08 13:04:35 -0500

    Added package.json file also in index.js file

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import express from "express";
 import cors from "cors";
 import directorsRouter from "./routers/directors.js";
 import moviesRouter from "./routers/movies.js";
+
+app.use(express.json()); //to parse our JSON body for POST requests
+app.use(cors()); //add CORS headers on all routes
