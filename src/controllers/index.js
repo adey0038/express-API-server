cb730bb0dde0e4f8ce2d8842923d2c9b4aa8a236
Oluwafunke Adeyemo <adey0038@algonquinlive.com>
2026-02-08 17:23:18 -0500

    Completed directors API endpoint

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,15 +8,70 @@ function getAllDirectors(req, res) {
   });
 }
 
-function getOneDirector(req, res) {}
+function getOneDirector(req, res) {
+  let director = Directors.find((d) => d.id === req.params.id);
+  if (director) {
+    res.json({
+      message: "Found matching director",
+      director,
+    });
+  } else {
+    res.status(404).json({
+      message: `Director with id ${req.params.id} not found`,
+    });
+  }
+}
 
-function addDirector(req, res) {}
+function addDirector(req, res) {
+  let d = {
+    id: crypto.randomUUID(),
+    name: req.body.name,
+    movies: [],
+  };
+  Directors.push(d);
+  res.status(201).json({
+    message: "Added one director",
+    director: d,
+  });
+}
 
-function putDirector(req, res) {}
+function putDirector(req, res) {
+  let director = Directors.find((d) => d.id === req.params.id);
+  if (director) {
+    director.name = req.body.name;
+    res.json({ message: "Replaced director", director });
+  } else {
+    res
+      .status(404)
+      .json({ message: `Director with id ${req.params.id} not found` });
+  }
+}
 
-function patchDirector(req, res) {}
+function patchDirector(req, res) {
+  let dir = Directors.find((d) => d.id === req.params.id);
+  if (dir) {
+    if (req.params.id) {
+      dir.name = req.body.name;
+    }
+    res.json({ message: "Directors updated", dir });
+  } else {
+    res
+      .status(404)
+      .json({ message: `Director with id ${req.params.id} not found` });
+  }
+}
 
-function deleteDirector(req, res) {}
+function deleteDirector(req, res) {
+  let index = Directors.findIndex((d) => d.id === req.params.id);
+  if (index !== -1) {
+    Directors.splice(index, 1);
+    res.json({ message: "Director deleted" });
+  } else {
+    res
+      .status(404)
+      .json({ message: `Director with id ${req.params.id} not found` });
+  }
+}
 
 //Movies
 
