065d829ebda6827ce32569e9f1e1253ecaaaf3f0
Oluwafunke Adeyemo <adey0038@algonquinlive.com>
2026-02-08 20:24:24 -0500

    Complete Movie API

---
+++ readme.md
@@ -0,0 +1,76 @@
+# Directors and Movies API
+
+This is a simple Express API to manage movie directors and their movies. It uses a static JSON model and follows best practices with separate controllers and routers.
+
+---
+
+## API Base URL
+
+All endpoints are prefixes with `/api`.
+http://localhost:3000/api
+
+---
+
+## Directors Endpoints
+
+### Get all directors
+
+**GET** `/directors`
+
+**Example:**
+
+```bash
+http://localhost:3000/api/directors
+```
+
+### Get a single director
+
+**GET** `/directors/:id`
+
+### Add a director
+
+**POST** `/directors`
+
+### Replace a director
+
+**PUT** `/directors/:id`
+
+### Update a director
+
+**PATCH** `/directors/:id`
+
+### Delete a director
+
+**DELETE** `/directors/:id`
+
+## Movies Endpoints
+
+### Get all movies from one director
+
+**GET** `/directors/:id/movies`
+
+**Example:**
+
+```bash
+http://localhost:3000/api/directors/:id/movies
+```
+
+### Get a single movie from one director
+
+**GET** `/directors/:id/movies/:mid`
+
+### Add a movie to a specific director
+
+**POST** `/directors/:id/movies`
+
+### Replace a movie
+
+**PUT** `/directors/:id/movies/:mid`
+
+### Update a movie
+
+**PATCH** `/directors/:id/movies/:mid`
+
+### Delete a movie
+
+**DELETE** `/directors/:id/movies/:mid`
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -30,7 +30,7 @@ function addDirector(req, res) {
   };
   Directors.push(d);
   res.status(201).json({
-    message: "Added one director",
+    message: "Director added",
     director: d,
   });
 }
@@ -53,12 +53,16 @@ function putDirector(req, res) {
 
 function patchDirector(req, res) {
   let index = Directors.findIndex((d) => d.id === req.params.id);
+
   if (index < 0) {
     res.status(404).json({
       message: `Director with id ${req.params.id} not found`,
     });
   }
+
+  // prevent ID from being overwritten
   const { id: _id, ...updates } = req.body;
+
   Directors[index] = {
     ...Directors[index],
     ...updates,
@@ -82,18 +86,139 @@ function deleteDirector(req, res) {
 }
 
 //Movies
+function getAllMovies(req, res) {
+  let dir = Directors.find((d) => d.id === req.params.id);
+  if (dir) {
+    res.json({
+      message: "All movies retrieved",
+      movies: dir.movies,
+    });
+  } else {
+    res
+      .status(404)
+      .json({ message: `Director with id ${req.params.id} not found` });
+  }
+}
 
-function getAllMovies(req, res) {}
+function getOneMovie(req, res) {
+  let dir = Directors.find((d) => d.id === req.params.id);
+  if (dir) {
+    let movie = dir.movies.find((m) => m.id === req.params.mid);
 
-function getOneMovie(req, res) {}
+    if (movie) {
+      res.json({
+        message: "Matching movie found",
+        movie,
+      });
+    } else {
+      res.status(404).json({
+        message: `Movie with id ${req.params.mid} not found`,
+      });
+    }
+  } else {
+    res
+      .status(404)
+      .json({ message: `Director with id ${req.params.id} not found` });
+  }
+}
 
-function addMovie(req, res) {}
+function addMovie(req, res) {
+  let dir = Directors.find((d) => d.id === req.params.id);
+  if (!dir) {
+    res
+      .status(404)
+      .json({ message: `Director with id ${req.params.id} not found` });
+  }
 
-function deleteMovie(req, res) {}
+  let m = {
+    id: crypto.randomUUID(),
+    title: req.body.title,
+  };
+  dir.movies.push(m);
+  res.status(201).json({
+    message: "Movie added",
+    movie: m,
+  });
+}
 
-function putMovie(req, res) {}
+function putMovie(req, res) {
+  let directorIndex = Directors.findIndex((d) => d.id === req.params.id);
 
-function patchMovie(req, res) {}
+  if (directorIndex < 0) {
+    res
+      .status(404)
+      .json({ message: `Director with id ${req.params.id} not found` });
+  }
+
+  const movieIndex = Directors[directorIndex].movies.findIndex(
+    (m) => m.id === req.params.mid,
+  );
+  if (movieIndex < 0) {
+    res.status(404).json({
+      message: `Movie with id ${req.params.mid} not found for director ${req.params.id}`,
+    });
+  }
+
+  const newMovie = {
+    id: Directors[directorIndex].movies[movieIndex].id,
+    title: req.body.title ?? "",
+  };
+
+  Directors[directorIndex].movies[movieIndex] = newMovie;
+  res.json({
+    message: "Replaced movie",
+    movie: newMovie,
+  });
+}
+
+function patchMovie(req, res) {
+  const directorIndex = Directors.findIndex((d) => d.id === req.params.id);
+
+  if (directorIndex < 0) {
+    res
+      .status(404)
+      .json({ message: `Director with id ${req.params.id} not found` });
+  }
+
+  const movies = Directors[directorIndex].movies;
+  const movieIndex = movies.findIndex((m) => m.id === req.params.mid);
+
+  if (movieIndex < 0) {
+    res
+      .status(404)
+      .json({ message: `Movie with id ${req.params.mid} not found` });
+  }
+
+  const { id: _id, ...updates } = req.body;
+  movies[movieIndex] = {
+    ...movies[movieIndex],
+    ...updates,
+  };
+  res.json({
+    message: "Movie updated",
+    movie: movies[movieIndex],
+  });
+}
+
+function deleteMovie(req, res) {
+  let dir = Directors.find((d) => d.id === req.params.id);
+
+  if (dir) {
+    let index = dir.movies.findIndex((m) => m.id === req.params.mid);
+    if (index !== -1) {
+      dir.movies.splice(index, 1);
+      res.json({ message: "Movie deleted" });
+    } else {
+      res
+        .status(404)
+        .json({ message: `Movie with id ${req.params.mid} not found` });
+    }
+  } else {
+    res
+      .status(404)
+      .json({ message: `Director with id ${req.params.id} not found` });
+  }
+}
 
 export {
   getAllDirectors,
